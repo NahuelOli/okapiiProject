19e9962ff11ae56aef73568d64ea5c179ac8485f
NahueOli <nahuolivera55@gmail.com>
2023-05-31 01:12:24 -0300

    Se añade la carpeta diagramas y main al repositorio.

---
+++ main/src/main/BaseDeUsuarios.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import java.util.ArrayList;
+
+public class BaseDeUsuarios {
+    ArrayList<Usuario> u;
+
+    public BaseDeUsuarios() {
+       u = new ArrayList<>();
+    }
+}
---
+++ main/src/main/Sistema.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Sistema {
+
+    private BaseDeUsuarios b;
+    private BaseDeProyectos p;
+    
+    
+    void arrancar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
