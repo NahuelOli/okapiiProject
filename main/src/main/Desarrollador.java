48934ec578f7d0b471332bbdf6e55358c92bcfc3
lucho-peluso <pelu.lucho@gmail.com>
2023-06-21 16:19:57 -0300

    Se agrego la clase desarrollador

---
+++ main/src/main/Desarrollador.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Desarrollador {
+    private String nombre;
+
+    public Desarrollador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+}
